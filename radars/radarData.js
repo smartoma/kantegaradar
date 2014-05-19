2c870bd5f1ff46bad8c27c6bc9077ef9ca813cad
Torstein Strøm <torstein.strom@gmail.com>
2014-05-19 16:27:34 +0200

    Oppdaterte med noen nye teknologier mm etter første fellesmøte mandag 19.mai 2014

--- radars/radarData.js
+++ radars/radarData.js
@@ -4,10 +4,12 @@ document.title = "";
 
 //This is the concentic circles that want on your radar
 var radar_arcs = [
-  {'r':120,'name':'Bruk'}
-  ,{'r':240,'name':'Vurder'}
-  ,{'r':360,'name':'Avstå'}
-  // ,{'r':500,'name':'Possible Extra if you want it'}
+    {'r': 120, 'name': 'Bruk'}
+    ,
+    {'r': 240, 'name': 'Vurder'}
+    ,
+    {'r': 360, 'name': 'Avstå'}
+    // ,{'r':500,'name':'Possible Extra if you want it'}
 ];
 
 //This is your raw data
@@ -21,6 +23,8 @@ var radar_arcs = [
 // blipSize:
 //  intValue; This is optional, if you omit this property, then your blip will be size 70.
 //            This give you the ability to be able to indicate information by blip size too
+//
+// comment; Optional, will be rendered as tooltip together with name
 //
 // url:
 // StringValue : This is optional, If you add it then your blips will be clickable to some URL
@@ -48,57 +52,68 @@ var h = 900;
 var w = 1200;
 
 var radar_data = [
-  { "quadrant": "Teknikker",
-    "left" : 45,
-    "top" : 18,
-    "color" : "#8FA227",
-    "items" : [
-      {"name":"Continuous delivery", "pc":{"r":80,"t":135},"movement":"c"},
-    ]
-  },
-  { "quadrant": "Verktøy",
-    "left": w-200+30,
-    "top" : 18,
-    "color" : "#587486",
-    "items" : [
-      {"name":"Subversion", "pc":{"r":340,"t":30},"movement":"c"},
-      {"name":"Git", "pc":{"r":70,"t":50},"movement":"c"},
-      {"name":"Ant", "pc":{"r":350,"t":20},"movement":"c"},
-      {"name":"Maven", "pc":{"r":70,"t":20},"movement":"c"},
-      {"name":"Grunt", "pc":{"r":140,"t":20},"movement":"c"},
-      {"name":"Gradle", "pc":{"r":100,"t":30},"movement":"c"},
-      {"name":"NoSQL", "pc":{"r":170,"t":70},"movement":"c"},
-    ]
-  },
-  { "quadrant": "Plattformer",
-    "left" :45,
-    "top" : (h/2 + 18),
-    "color" : "#DC6F1D",
-    "items" : [
-      {"name":"JSP", "pc":{"r":320,"t":265},"movement":"c"},
-      {"name":"JSF", "pc":{"r":310,"t":235},"movement":"c"},
-      {"name":"Struts", "pc":{"r":350,"t":255},"movement":"c"},
-      {"name":"Wicket", "pc":{"r":120,"t":255},"movement":"c"},
-      {"name":"Node.JS", "pc":{"r":150,"t":255},"movement":"c"},
-      {"name":"Lettvektskontainer (f.eks Jetty)", "pc":{"r":80,"t":255},"movement":"c"},
-      {"name":"Full applikasjonsserver", "pc":{"r":290,"t":255},"movement":"c"},
-      {"name":"Hibernate/JPA", "pc":{"r":190,"t":200},"movement":"t"},
-      {"name":"Play Scala/Java", "pc":{"r":170,"t":190},"movement":"c"},
-      {"name":"Angular JS", "pc":{"r":170,"t":220},"movement":"c"},
-      {"name":"Ember.JS", "pc":{"r":170,"t":230},"movement":"c"},
-    ]
-  },
-  { "quadrant": "Språk",
-    "color" : "#B70062",
-    "left"  : w-200+30,
-    "top" :   (h/2 + 18),
-    "items" : [
-      {"name":"JavaScript", "pc":{"r":60,"t":325},"movement":"t"},
-      {"name":"Java", "pc":{"r":80,"t":345},"movement":"t"},
-      {"name":"Java8", "pc":{"r":180,"t":355},"movement":"t"},
-      {"name":"Scala", "pc":{"r":200,"t":345},"movement":"c"},
-      {"name":"Ruby", "pc":{"r":200,"t":320},"movement":"c"},
-      {"name":"Actionscript", "pc":{"r":320,"t":345},"movement":"t"},
-    ]
-  }
+    { "quadrant": "Teknikker",
+        "left": 45,
+        "top": 18,
+        "color": "#8FA227",
+        "items": [
+            {"name": "Continuous delivery", "pc": {"r": 80, "t": 135}, "movement": "c"},
+            {"name": "BigData", "pc": {"r": 150, "t": 155}, "movement": "c"},
+            {"name": "SOA", "pc": {"r": 230, "t": 145}, "movement": "c"},
+            {"name": "Soap", "pc": {"r": 210, "t": 135}, "movement": "c"},
+            {"name": "JSON", "pc": {"r": 80, "t": 100   }, "movement": "c"},
+        ]
+    },
+    { "quadrant": "Verktøy",
+        "left": w - 200 + 30,
+        "top": 18,
+        "color": "#587486",
+        "items": [
+            {"name": "Subversion", "pc": {"r": 340, "t": 30}, "movement": "c"},
+            {"name": "Git", "pc": {"r": 70, "t": 50}, "movement": "c"},
+            {"name": "Ant", "pc": {"r": 350, "t": 20}, "movement": "c"},
+            {"name": "Maven", "pc": {"r": 70, "t": 20}, "movement": "c"},
+            {"name": "Grunt", "pc": {"r": 140, "t": 20}, "movement": "c"},
+            {"name": "Gradle", "pc": {"r": 100, "t": 30}, "movement": "c"},
+            {"name": "NoSQL", "pc": {"r": 170, "t": 70}, "movement": "c"},
+            {"name": "Node.JS som støtteverktøy", "pc": {"r": 150, "t": 255}, "movement": "c"},
+        ]
+    },
+    { "quadrant": "Rammeverk",
+        "left": 45,
+        "top": (h / 2 + 18),
+        "color": "#DC6F1D",
+        "items": [
+            {"name": "JSP", "pc": {"r": 320, "t": 265}, "movement": "c"},
+            {"name": "JSF", "pc": {"r": 310, "t": 235}, "movement": "c"},
+            {"name": "Struts", "pc": {"r": 350, "t": 255}, "movement": "c"},
+            {"name": "Wicket", "pc": {"r": 120, "t": 255}, "movement": "c", "url":"http://wicket.apache.org/", "comment":"Vi har mye kompetanse på Wicket"},
+            {"name": "Node.JS som appserver", "pc": {"r": 150, "t": 255}, "movement": "c"},
+            {"name": "Lettvektskontainer (f.eks Jetty)", "pc": {"r": 80, "t": 255}, "movement": "c"},
+            {"name": "Full applikasjonsserver", "pc": {"r": 290, "t": 255}, "movement": "c"},
+            {"name": "Hibernate/JPA", "pc": {"r": 190, "t": 200}, "movement": "t"},
+            {"name": "Play Scala/Java", "pc": {"r": 170, "t": 190}, "movement": "c"},
+            {"name": "Angular JS", "pc": {"r": 170, "t": 220}, "movement": "c"},
+            {"name": "Ember.JS", "pc": {"r": 175, "t": 230}, "movement": "c"},
+            {"name": "Spring Core", "pc": {"r": 170, "t": 255}, "movement": "c"},
+            {"name": "Spring MVC", "pc": {"r": 200, "t": 260}, "movement": "c"},
+            {"name": "Spring JDBC", "pc": {"r": 190, "t": 240}, "movement": "c"},
+            {"name": "Storm", "pc": {"r": 195, "t": 230}, "movement": "c"},
+            {"name": "Akka", "pc": {"r": 170, "t": 200}, "movement": "c"},
+            {"name": "Hadoop", "pc": {"r": 210, "t": 200}, "movement": "c"},
+        ]
+    },
+    { "quadrant": "Språk",
+        "color": "#B70062",
+        "left": w - 200 + 30,
+        "top": (h / 2 + 18),
+        "items": [
+            {"name": "JavaScript", "pc": {"r": 60, "t": 325}, "movement": "t"},
+            {"name": "Java", "pc": {"r": 80, "t": 345}, "movement": "t"},
+            {"name": "Java8", "pc": {"r": 180, "t": 355}, "movement": "t"},
+            {"name": "Scala", "pc": {"r": 200, "t": 345}, "movement": "c"},
+            {"name": "Ruby", "pc": {"r": 200, "t": 320}, "movement": "c"},
+            {"name": "Actionscript", "pc": {"r": 320, "t": 345}, "movement": "t"},
+        ]
+    }
 ];
